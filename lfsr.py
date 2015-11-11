2bdb5334dfa95f4d94b1098035465a2914c44432
Sam G <thegladiusmaximus@gmail.com>
2015-11-10 22:51:44 -0600

    Update lfsr.py
    I like the generator better than the object-oriented

--- lfsr.py
+++ lfsr.py
@@ -1,90 +1,125 @@
 #!/usr/bin/env python3
-class LFSR:
-    def __init__(self, seed, taps):
-        self.seed(seed)
-        self.taps(taps)
-        for x in self.taps:
-            if x > len(self.seed):
-                raise RuntimeError('Tried to tap a nonexistent bit')
+import itertools
 
-    def seed(self, seed):
-        if type(seed) is int:
-            self.seed = list(map(int, str(seed)))
-            self.contains = 'int'
-        elif type(seed) is str:
-            self.seed = list(seed)
-            self.contains = 'str'
-        else:
-            raise RuntimeError('Unexpected Seed Input')
-        
-    def taps(self, taps):
-        if type(taps) is list:
-            self.taps = taps
-        else:
-            raise RuntimeError('Taps type should be list')
-    
-    def next(self):
-        self.hide_seed()
-        if self.contains == 'str':
-            self.iterate_string()
-            return self.output
-        elif self.contains == 'int':
-            self.iterate_integer()
-            return self.output
-        else:
-            raise RuntimeError('Unexpected Seed Input')
+#### Object-oriented LFSR
+class LFSRClass:
+    def __init__(self, seed, taps, tapfunc=None):
+        self.seed = seed
+        self.taps = taps
+        if tapfunc is None:
+            self.tapfunc = default_tapfunc
 
-    def hide_seed(self):
-        if self.contains == 'str':
-            for x in range (1, len(self.seed) + 1):
-                self.iterate_string()
-        elif self.contains == 'int':
-            for x in range (1, len(self.seed) + 1):
-                self.iterate_integer()
+    def setSeed(self, seed):
+        # self._seed is the actual seed
+        if isinstance(seed, int):
+            self._seed = list(map(int, str(seed)))
         else:
-            raise RuntimeError('Unexpected Seed Input')
+            # http://stackoverflow.com/questions/1952464/in-python-how-do-i-determine-if-an-object-is-iterable
+            try:
+                self._seed = list(iter(seed))
+            except TypeError: # this TypeError exception says 'not an iterator'
+                # I want one that says something else
+                raise TypeError('Seed must be an integer or an iterable')
 
-    def iterate_string(self):
-        if self.contains == "str":
-            self.output = self.seed[len(self.seed) - 1]
-            xor = ord(self.seed[self.taps[0] - 1])
-            for x in range(1, len(self.taps)):
-                xor = xor ^ ord(self.seed[self.taps[x] - 1])
-            for x in range(1,len (self.seed) + 1):
-                self.seed[len(self.seed) - x] = self.seed[len(self.seed) - x - 1]
-            self.seed[0] = chr(xor)
-        else:
-            raise RuntimeError("Can't call on non-string list")
-        
-    def iterate_integer(self):
-        if self.contains == "int":
-            self.output = self.seed[len(self.seed) - 1]
-            xor = self.seed[self.taps[0] - 1]
-            for x in range(1, len(self.taps)):
-                xor = xor ^ self.seed[self.taps[x] - 1]
-            for x in range(1, len(self.seed) + 1):
-                self.seed[len(self.seed) - x] = self.seed[len(self.seed) - x - 1]
-            self.seed[0] = xor
-            return output
-        else:
-            raise RuntimeError("Can't call on non-integer list")
+    def getSeed(self):
+        return self._seed
 
-    def state(self):
-        return self.seed
+    # self.seed is what it it appears to the outside
+    seed = property(getSeed, setSeed)
+
+    def setTaps(self, taps):
+        for tap in taps:
+            if tap >= len(self.seed):
+                raise RuntimeError('Tried to tap non-existant bit')
+        self._taps = taps
+
+    def getTaps(self):
+        return self._taps
+
+    taps = property(getTaps, setTaps)
+
+    def __iter__(self):
+        # returns an iterable
+        # conveniently, self is an iterable
+        return self
+
+    def __next__(self):
+        a = self.seed[0]
+        b = 0
+        for tap in self.taps:
+            b = self.tapfunc(b, self.seed[tap])
+        # shift everything down by one
+        self.seed[:-1] = self.seed[1:]
+        self.seed[-1] = b
+        return a
 
     def display(self):
-        print(self.seed)
-        tapsdisplay = '['
-        for x in range(1, len(self.seed) + 1):
-            if x in self.taps:
-                tapsdisplay += 'T, '
-            else:
-                tapsdisplay += ' , '
-        tapsdisplay = tapsdisplay[:-2]
-        tapsdisplay += "]"
-        print(tapsdisplay)
+        output = [' '] * len(self.seed)
+        for tap in self.taps:
+            output[tap] = 'T'
+        return ', '.join(output)
 
-x = LFSR("ABCEFGH", [5])
-for y in range(0, 20):
-    print(x.next())
-x.display()
+def demo1():
+    a = LFSRClass([1, 0, 0, 0], [0, 1])
+    for n, r in zip(range(10), a):
+        # get the first ten results
+        print('{0}: {1}'.format(n, r))
+    print()
+    a = LFSRClass(map(int, '1000'), [0, 1])
+    ctr = 0
+    for r in itertools.islice(a, 10):
+        # another way to get the ten
+        print('{0}: {1}'.format(ctr, r))
+
+##### Generator based LFSR
+def LFSRFunction(seed, taps, tapfunc=None):
+    # not so object oriented
+    if isinstance(seed, int):
+        seed = map(int, str(seed))
+    else:
+        # http://stackoverflow.com/questions/1952464/in-python-how-do-i-determine-if-an-object-is-iterable
+        try:
+            seed = list(iter(seed))
+        except TypeError: # this TypeError exception says 'not an iterator'
+            # I want one that says something else
+            raise TypeError('Seed must be an integer or an iterable')
+
+    if tapfunc == None:
+        tapfunc = default_tapfunc
+
+    while True:
+        a = seed[0]
+        b = 0
+        for tap in taps:
+            b = tapfunc(b, seed[tap])
+        seed[:-1] = seed[1:]
+        seed[-1] = b
+        yield a
+
+def LFSRDisplay(taps, n):
+    output = [' '] * n
+    for tap in self.taps:
+        output[tap] = 'T'
+    return ', '.join(output)
+
+def demo2():
+    a = LFSRFunction([1, 0, 0, 0], [0, 1])
+    ctr = 0
+    for r in a:
+        print('{ctr}: {r}'.format(ctr=ctr, r=r))
+        ctr += 1
+        if ctr >= 10:
+            break
+    print()
+    a = LFSRFunction([1, 0, 0, 0], [0, 1])
+    for n, r in zip(range(10), a):
+        print('{n}: {r}'.format(**locals()))
+
+
+###### Helper function
+def default_tapfunc(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a ^ b
+    if isinstance(a, str) and isinstance(b, str):
+        return chr(ord(a) ^ ord(b))
+    raise TypeError("The default tap function doesn't know how to xor the types: {0!s} and {1!s}".format(type(a), type(b)))
